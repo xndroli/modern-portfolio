f31b77f3e0831ea89c896d5d7fe09afc8132a806
xndroli <103154250+xndroli@users.noreply.github.com>
2025-04-16 15:40:47 -0400

    created Hero Text & Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
     return (
-        <main>
-            <div>
-                <h1>Welcome to my 3D Portfolio</h1>
-            </div>
-        </main>
+        <>
+            <Hero />
+        </>
     )
-};
+}
 
-export default App;
+export default App
